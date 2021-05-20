cd507df9b8fd0e4e87f199039fc730e4ceee2b4d
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-05-20 17:45:28 +0600

    Updated: Button hover

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+function closePopup() {
+    document.getElementById('process').style.display='none'
+}
+
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
